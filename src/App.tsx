e4424d6ccb65cfb17f182e57cd583fdc172aac56
Tim <tmclough98@gmail.com>
2021-12-13 22:30:14 -0500

    Add highlight region support

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,21 @@ import './App.css';
 
 const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
 
+// Represents a highlighted region of the staff
+interface HighlightRegion {
+  startLine: number;
+  startColumn: number;
+  endLine: number;
+  endColumn: number;
+}
+
+// A place on a staff plus content for that place
+interface Note {
+  text: string;
+  line: number;
+  column: number;
+}
+
 // Beginning of In Your Eyes by BADBADNOTGOOD
 function exampleSong(): Note[] {
   const stringSpec = {
@@ -19,90 +34,148 @@ function exampleSong(): Note[] {
   return parseSpec(spec, stringSpec);
 }
 
-export function App() {
-  const [currentPosition, setCurrentPosition] = useState({
-    text: '',
-    line: 0,
-    position: 0,
-  });
+function fallsWithinRegion(
+  region: HighlightRegion,
+  line: number,
+  column: number,
+): boolean {
+  if (
+    region.startLine <= line &&
+    line <= region.endLine &&
+    region.startColumn <= column &&
+    column <= region.endColumn
+  ) {
+    return true;
+  }
 
+  if (
+    region.endLine <= line &&
+    line <= region.startLine &&
+    region.endColumn <= column &&
+    column <= region.startColumn
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+export function App() {
+  // Staff parameters
   const lineCount = 4,
     noteWidth = 18,
     noteCountPerLine = 40,
     padding = 20,
     margin = 10;
 
-  const [notes, setNotes] = useState(exampleSong());
+  const [isMouseDown, setIsMouseDown] = useState(false);
 
+  const [highlightRegion, setHighlightRegion] =
+    useState<HighlightRegion | null>(null);
+
+  const [currentNote, setCurrentNote] = useState<Note>({
+    text: '',
+    line: 0,
+    column: 0,
+  });
+
+  const [notes, setNotes] = useState<Note[]>(exampleSong());
+
+  // Handle staff and edit events
   const keyDownHandler = (
     event: React.KeyboardEvent<HTMLDivElement>,
   ) => {
     if (event.code === 'ArrowUp') {
-      if (currentPosition.text) {
-        setNotes([...notes, { ...currentPosition }]);
+      if (currentNote.text) {
+        setNotes([...notes, { ...currentNote }]);
       }
 
-      setCurrentPosition({
+      setCurrentNote({
         text: '',
-        line: (currentPosition.line - 1 + lineCount) % lineCount,
-        position: currentPosition.position,
+        line: (currentNote.line - 1 + lineCount) % lineCount,
+        column: currentNote.column,
       });
     } else if (event.code === 'ArrowDown') {
-      if (currentPosition.text) {
-        setNotes([...notes, { ...currentPosition }]);
+      if (currentNote.text) {
+        setNotes([...notes, { ...currentNote }]);
       }
 
-      setCurrentPosition({
+      setCurrentNote({
         text: '',
-        line: (currentPosition.line + 1) % lineCount,
-        position: currentPosition.position,
+        line: (currentNote.line + 1) % lineCount,
+        column: currentNote.column,
       });
     } else if (event.code === 'ArrowLeft') {
-      if (currentPosition.text) {
-        setNotes([...notes, { ...currentPosition }]);
+      if (currentNote.text) {
+        setNotes([...notes, { ...currentNote }]);
       }
 
-      setCurrentPosition({
+      setCurrentNote({
         text: '',
-        line: currentPosition.line,
-        position: currentPosition.position - 1,
+        line: currentNote.line,
+        column: currentNote.column - 1,
       });
     } else if (event.code === 'ArrowRight') {
-      if (currentPosition.text) {
-        setNotes([...notes, { ...currentPosition }]);
+      if (currentNote.text) {
+        setNotes([...notes, { ...currentNote }]);
       }
 
-      setCurrentPosition({
+      setCurrentNote({
         text: '',
-        line: currentPosition.line,
-        position: currentPosition.position + 1,
+        line: currentNote.line,
+        column: currentNote.column + 1,
       });
     }
 
     if (event.key.match(/\d/)) {
       const newText =
-        currentPosition.text.length > 2
+        currentNote.text.length > 1
           ? event.key
-          : currentPosition.text + event.key;
+          : currentNote.text + event.key;
 
-      setCurrentPosition({
+      setCurrentNote({
         text: newText,
-        line: currentPosition.line,
-        position: currentPosition.position,
+        line: currentNote.line,
+        column: currentNote.column,
       });
     }
   };
 
+  const setHighlightRegionStart = (
+    line: number,
+    column: number,
+  ): void => {
+    setHighlightRegion({
+      startColumn: column,
+      startLine: line,
+      endColumn: column,
+      endLine: line,
+    });
+  };
+
+  const setHighlightRegionEnd = (
+    line: number,
+    column: number,
+  ): void => {
+    setHighlightRegion({
+      startColumn: highlightRegion?.startColumn ?? column,
+      startLine: highlightRegion?.startLine ?? line,
+      endColumn: column,
+      endLine: line,
+    });
+  };
+
+  // Callback to update the active position
   const setPosition = (
     staffLine: number,
     line: number,
     position: number,
   ) => {
     const absolutePosition = staffLine * noteCountPerLine + position;
-    setCurrentPosition({
+    setCurrentNote({
       text: '',
       line: line,
-      position: absolutePosition,
+      column: absolutePosition,
     });
   };
 
@@ -115,8 +188,13 @@ export function App() {
         noteWidth={noteWidth}
         noteCountPerLine={noteCountPerLine}
         notes={notes}
-        currentPosition={currentPosition}
+        currentPosition={currentNote}
+        highlightedRegion={highlightRegion ?? undefined}
+        isMouseDown={isMouseDown}
+        setIsMouseDown={setIsMouseDown}
         setPosition={setPosition}
+        setHighlightRegionStart={setHighlightRegionStart}
+        setHighlightRegionEnd={setHighlightRegionEnd}
       />
     </div>
   );
@@ -146,7 +224,7 @@ function parseSpec(spec: string, stringNames: StringSpec): Note[] {
       notes.push({
         text: fret,
         line: stringIndex,
-        position: noteIndex,
+        column: noteIndex,
       });
 
       noteIndex += noteLength;
@@ -158,12 +236,6 @@ function parseSpec(spec: string, stringNames: StringSpec): Note[] {
   return notes;
 }
 
-interface Note {
-  text: string;
-  line: number;
-  position: number;
-}
-
 interface StaffProps {
   padding: number;
   margin: number;
@@ -172,11 +244,16 @@ interface StaffProps {
   noteCountPerLine: number;
   notes: Note[];
   currentPosition?: Note;
+  highlightedRegion?: HighlightRegion;
+  isMouseDown: boolean;
+  setIsMouseDown(isMouseDown: boolean): void;
   setPosition(
     staffLine: number,
     line: number,
     position: number,
   ): void;
+  setHighlightRegionStart(line: number, column: number): void;
+  setHighlightRegionEnd(line: number, column: number): void;
 }
 
 function Staff(props: StaffProps) {
@@ -188,7 +265,12 @@ function Staff(props: StaffProps) {
     noteCountPerLine,
     notes,
     currentPosition,
+    highlightedRegion,
+    isMouseDown,
+    setIsMouseDown,
     setPosition,
+    setHighlightRegionStart,
+    setHighlightRegionEnd,
   } = props;
 
   // Position notes on separate staff lines
@@ -196,30 +278,16 @@ function Staff(props: StaffProps) {
   for (let note of notes) {
     note = { ...note };
 
-    const staffLine = Math.floor(note.position / noteCountPerLine);
-    const staffPosition = note.position % noteCountPerLine;
+    const staffLine = Math.floor(note.column / noteCountPerLine);
+    const staffPosition = note.column % noteCountPerLine;
 
     if (!staffLines[staffLine]) {
       staffLines[staffLine] = [];
     }
 
-    note.position = staffPosition;
+    note.column = staffPosition;
 
     staffLines[staffLine].push(note);
-  }
-
-  // Figure out which staff line active position goes on
-  let positionStaffLine: number | undefined,
-    staffLineCurrentPosition: Note | undefined;
-  if (currentPosition) {
-    positionStaffLine = Math.floor(
-      currentPosition.position / noteCountPerLine,
-    );
-    staffLineCurrentPosition = {
-      text: currentPosition.text,
-      line: currentPosition.line,
-      position: currentPosition.position % noteCountPerLine,
-    };
   }
 
   const staffLineElements = staffLines.map((staffLine, i) => (
@@ -228,14 +296,22 @@ function Staff(props: StaffProps) {
       padding={padding}
       margin={margin}
       lineCount={lineCount}
+      staffLineIndex={i}
       noteWidth={noteWidth}
       noteCount={noteCountPerLine}
       notes={staffLine}
-      currentPosition={
-        i === positionStaffLine ? staffLineCurrentPosition : undefined
-      }
+      currentPosition={currentPosition}
+      highlightedRegion={highlightedRegion}
+      isMouseDown={isMouseDown}
+      setIsMouseDown={setIsMouseDown}
       setPosition={(line, position) => {
         setPosition(i, line, position);
+      }}
+      setHighlightRegionStart={(line, column) => {
+        setHighlightRegionStart(line, column + i * noteCountPerLine);
+      }}
+      setHighlightRegionEnd={(line, column) => {
+        setHighlightRegionEnd(line, column + i * noteCountPerLine);
       }}
     />
   ));
@@ -250,12 +326,18 @@ function Staff(props: StaffProps) {
 interface StaffLineProps {
   padding: number;
   margin: number;
+  staffLineIndex: number;
   lineCount: number;
   noteWidth: number;
   noteCount: number;
   notes: Note[];
   currentPosition?: Note;
+  highlightedRegion?: HighlightRegion;
+  isMouseDown: boolean;
+  setIsMouseDown(isMouseDown: boolean): void;
   setPosition(line: number, position: number): void;
+  setHighlightRegionStart(line: number, column: number): void;
+  setHighlightRegionEnd(line: number, column: number): void;
 }
 
 function StaffLine(props: StaffLineProps) {
@@ -263,11 +345,17 @@ function StaffLine(props: StaffLineProps) {
     padding,
     margin,
     lineCount,
+    staffLineIndex,
     noteWidth,
     noteCount,
     notes,
     currentPosition,
+    highlightedRegion,
+    isMouseDown,
+    setIsMouseDown,
     setPosition,
+    setHighlightRegionStart,
+    setHighlightRegionEnd,
   } = props;
 
   const height = 2 * margin + (lineCount - 1) * padding;
@@ -276,19 +364,19 @@ function StaffLine(props: StaffLineProps) {
   const visibleElements: JSX.Element[][] = [];
 
   for (let note of notes) {
-    const x = note.position * noteWidth;
+    const x = note.column * noteWidth;
     const y = note.line * padding + margin + 5; // TODO magic number
 
     if (!visibleElements[note.line]) {
       visibleElements[note.line] = [];
     }
 
-    visibleElements[note.line][note.position] = (
+    visibleElements[note.line][note.column] = (
       <text
         className="staffElement"
         x={x}
         y={y}
-        key={note.line * noteCount + note.position}
+        key={note.line * noteCount + note.column}
       >
         {note.text}
       </text>
@@ -306,7 +394,7 @@ function StaffLine(props: StaffLineProps) {
 
       const isSelected =
         i === currentPosition?.line &&
-        j === currentPosition?.position;
+        j + staffLineIndex * noteCount === currentPosition?.column;
 
       if (isSelected && currentPosition?.text && visibleElements[i]) {
         if (!visibleElements[i]) {
@@ -344,18 +432,61 @@ function StaffLine(props: StaffLineProps) {
         setPosition(i, j);
       };
 
-      const boxClass = isSelected ? 'staffBox selected' : 'staffBox';
+      const mouseDownHandler = (event: React.MouseEvent) => {
+        setHighlightRegionStart(i, j);
+        setIsMouseDown(true);
+      };
+
+      const mouseUpHandler = (event: React.MouseEvent) => {
+        setIsMouseDown(false);
+      };
+
+      const mouseEnterHandler = (event: React.MouseEvent) => {
+        if (!isMouseDown) {
+          return;
+        }
+
+        if (!(event.target instanceof SVGRectElement)) {
+          return;
+        }
+
+        let line = parseInt(event.target.dataset.line ?? ''),
+          column = parseInt(event.target.dataset.column ?? '');
+
+        setHighlightRegionEnd(line, column);
+      };
+
+      let boxClass = 'staffBox';
+      if (isSelected) {
+        boxClass += ' selected';
+      }
+
+      if (
+        highlightedRegion &&
+        fallsWithinRegion(
+          highlightedRegion,
+          i,
+          j + noteCount * staffLineIndex,
+        )
+      ) {
+        boxClass += ' highlighted';
+      }
+
       boxes.push(
         <rect
           key={i * noteCount + j}
           className={boxClass}
           x={x}
-          y={y - 8} // TODO magic number
+          y={y - padding / 2}
           width={noteWidth}
-          height={15} // TODO magic number
-          stroke="black"
-          strokeWidth="1"
+          height={padding}
+          stroke="none"
           onClick={onBoxClick}
+          onMouseDown={mouseDownHandler}
+          onMouseUp={mouseUpHandler}
+          onMouseEnter={mouseEnterHandler}
+          data-line={i}
+          data-column={j}
         />,
       );
     }
